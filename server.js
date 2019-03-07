aa0f0bdf0be432f577dce5615397e8beb9721160
morgan-gill <32048628+morgan-gill@users.noreply.github.com>
2019-03-06 23:20:39 -0500

    working on the socket.io portion
    attempted to follow the outline of the video provided. The application doesn't work but with a little meeting time and coordination I think that we can get it working.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./backend/app");
 const http = require('http');
+const io = require('socket.io');
+// const sapp = require("./socketFiles/sapp");
 
+const user = io.listen(3000).sockets;
 const hostname = '127.0.0.1';
 const port = process.env.PORT || "3000";
 
@@ -9,4 +12,69 @@ app.set("port", port);
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+
+  user.on('connction', function(socket) {
+
+    // let chat = db.collection('chats'); This line was from the video
+
+    // Sending status to client
+    status = (param) => {
+      socket.emit('status', param);
+    }
+
+    /*
+    // Get chats from mongo collection
+    chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+      if(err){
+        throw err;
+      }
+
+      // Emit the messages
+      socket.emit('output', res);
+    });
+    */
+
+    // Handle input events
+    socket.on('input', function(data){
+      let name = data.name;
+      let message = data.messge;
+
+      // Check for name and message
+      if (name == "" || message == "") {
+
+        // Send error status
+        status('please enter a name/message.');
+      } else {
+
+        // Insert message
+        chat.insert({name: name, message: message}, function(){
+          sio.emit('output', [data]);
+
+          // Send status object
+          status({
+            message: 'message sent',
+            clear: true
+          });
+
+        });
+
+      }
+
+    });
+
+    // Handle clear
+    socket.on('clear', function(data){
+
+      // remove all chats from the colelction
+      chat.remove({}, function(){
+
+        // Emit cleared
+        socket.emit('cleared');
+
+      });
+
+    });
+
+  });
+
 });
