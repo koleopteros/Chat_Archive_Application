4afc56416d350975157ed0627fc0df08400684a9
Morgan Gill <32048628+morgan-gill@users.noreply.github.com>
2019-03-08 15:27:35 -0500

    making sure that I don't over-write what was written
    making sure that I don't over-write what was written

--- public/chat.js
+++ public/chat.js
@@ -12,17 +12,17 @@ $(function(){
     // Emit message
     send_message.click(function(){
         socket.emit('new_message', {message : message.val()})
-    })
+    });
 
     // Listen on new_message
     socket.on("new_message", (data) => {
         console.log(data)
         chatroom.append("<p class='message'>" + data.username + ": " + data.message + "<p>")
-    })
+    });
 
     // Emit a username
     send_username.click(function(){
         console.log(username.val())
         socket.emit('change_username', {username : username.val()})
-    })
+    });
 });
